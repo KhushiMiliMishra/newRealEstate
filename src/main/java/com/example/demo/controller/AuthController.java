588ca65b9925abe730a2666ce478496fafc2b3ec
lavanya s <sec22cj004@sairamtap.edu.inC:/Users/sridh/AppData/Local/Programs/Python/Python313/python.exe>
2026-06-09 16:11:50 +0530

    Added user login and PostgreSQL integration

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.UserRequest;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/test")
+    public String test() {
+        return "Backend Working";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserRequest request) {
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        userRepository.save(user);
+
+        return "User Registered Successfully";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElse(null);
+
+        if (user == null) {
+            return "User Not Found";
+        }
+
+        if (user.getPassword().equals(request.getPassword())) {
+            return "Login Success";
+        }
+
+        return "Invalid Credentials";
+    }
+}
