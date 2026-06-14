41665d08bcd2835a97c9648c76dfeb9e9cdc5742
KhushiMiliMishra <khusmish1020@gmail.com>
2026-06-15 01:11:21 +0530

    App login and register added

---
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PropertyRequest;
+import com.example.demo.entity.Property;
+import com.example.demo.service.PropertyService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/properties")
+@CrossOrigin(origins = "")
+public class PropertyController {
+
+    @Autowired
+    private PropertyService propertyService;
+
+    @PostMapping
+    public Property createProperty(
+            @RequestBody PropertyRequest request) {
+
+        return propertyService.createProperty(request);
+    }
+
+    @GetMapping("/agent/{agentId}")
+    public List<Property> getPropertiesByAgent(
+            @PathVariable Long agentId) {
+
+        return propertyService.getPropertiesByAgent(agentId);
+    }
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyService.getAllProperties();
+    }
+
+    @GetMapping("/{id}")
+    public Property getPropertyById(
+            @PathVariable Long id) {
+
+        return propertyService.getPropertyById(id);
+    }
+}
