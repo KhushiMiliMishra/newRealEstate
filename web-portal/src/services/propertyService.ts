e378cbe496c54426f288efa78a94b990be164eba
KhushiMiliMishra <khusmish1020@gmail.com>
2026-06-15 12:31:16 +0530

    removed duplicate

--- web-portal/src/services/propertyService.ts
+++
@@ -1,9 +0,0 @@
-import API from "./api";
-
-export const createProperty = async (data: any) => {
-  return API.post("/properties", data);
-};
-
-export const getPropertiesByAgent = async (agentId: number) => {
-  return API.get(`/properties/agent/${agentId}`);
-};
